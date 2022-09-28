2c38111232e1892bace12ad8c5a872a3bd0b6bc1
Rendo-M <srvr@list.ru>
2022-09-28 16:20:42 +0300

    find text by len programm v 1.0

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿string[] CreateStringArray(int n)
+{
+    string [] newArr; 
+    string s = Console.ReadLine()!;
+    if (s != "")
+    {
+        newArr = CreateStringArray(n+1);
+        newArr[newArr.Length - n - 1] = s;
+    }
+    else 
+        newArr = new string [n];
+    return newArr;        
+}
+
+int CountSameSizeElement(string [] msg, int size)
+{
+    int count = 0;
+    for (int i = 0; i < msg.Length; i++)
+        count += Convert.ToByte(msg[i].Length == size);
+    return count;
+}
+
+string [] GetAllStringsBySize(string [] msg, int size)
+{
+    string [] result = new string[CountSameSizeElement(msg, size)];
+    int count  = 0;
+    for (int i = msg.Length-1; i >= 0; i--)
+        if (msg[i].Length == size)
+            {
+                result[count] = msg[i];
+                count++;
+            }
+    return result;
+}
+
+void PrintStringArray(string [] txt)
+{
+for (int i = 0; i < txt.Length; i++)
+Console.Write(txt[i]+" ");
+}
+
+
+Console.Clear();
+Console.WriteLine("Вводите строки текста. Признак окончания ввода - ввод пустой строки");
+int size = 3;
+string [] text = CreateStringArray(0);
+string [] threeSymbTexts = GetAllStringsBySize(text, size);
+Console.Write($"введены следующие {threeSymbTexts.Length} комбинации из {size} знаков: ");
+PrintStringArray(threeSymbTexts);
